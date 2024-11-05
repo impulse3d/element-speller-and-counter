d3b6333af114a2d355f1d0d573491c85a0b1724b
Josh Carroll <148890899+impulse3d@users.noreply.github.com>
2024-11-05 13:25:54 +0000

    Setup with text for user

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new streamlit app")
+st.title("Impulse 3d Prints Periodic Table Speller  🧪")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Enter the word or phrase you want to spell with the periodic table. Then copy the text into your personalization on our etsy page. Be sure to select the right number of tiles for your order!"
 )
